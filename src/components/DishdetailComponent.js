9858d478aee22b59f24b8af0eb114c0792729c52
mithelan <mithelandev@gmail.com>
2020-07-03 20:40:24 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,7 +13,7 @@ import {
 } from "reactstrap";
 import {Link} from 'react-router-dom'
 import {LocalForm,Control,Errors} from "react-redux-form";
-
+import { Loading } from './LoadingComponent';
 
 
 
@@ -79,6 +79,26 @@ const RenderComments=({comments,addComment,dishId})=> {
 
 
 const DishdetailComponent = (props) => {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+else
     if (props.dish != null) {
         return (
             <div className="container">
